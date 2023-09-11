8a1f424d2a126054aa4a487b31f8254a6478b740
Sabbir372569 <alamin228506@gmail.com>
2023-09-11 21:25:58 +0600

    six react core concept

--- src/Post.jsx
+++ src/Post.jsx
@@ -2,7 +2,7 @@ import './Post.css'
 export default function Post({post}){
     const{title, body, id, userId} = post;
     return (
-        <div className='post'> 
+        <div className='post-style'> 
             <p><small>User Id : {userId}</small></p>
             <p><small>Post Id : {id}</small></p>
             <h4>Title: {title}</h4>
